31b0646bcc0078f4b3093b3fc98c4ec85e91f2ba
rmvaldez14 <116440999+ronmiguelvaldez@users.noreply.github.com>
2023-08-18 14:35:30 +0800

    first

---
+++ src/components/Stats.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="stats">
+        <h1> A different component</h1>
+        <p>There are currently  {{  countLinks }} links</p>
+
+        <button @click="removeAllLinks"> Remove All Links</button>
+        <p> {{ msg }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import {mapGetters, mapMutations, mapActions} from 'vuex'
+  
+  export default {
+    name: 'Stats',
+    data() {
+        return {
+            msg: ''
+        }
+    },
+    computed: {
+      ...mapGetters([
+        'countLinks'
+      ]),
+    },
+    methods: {
+        ...mapMutations(['REMOVE_ALL']),
+        ...mapActions(['removeAll']),
+        removeAllLinks() {
+            this.removeAll().then(() => {
+                this.msg = 'They have been removed'
+            })
+        }
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  button {
+    padding: 10px;
+    margin-top: 30px;
+    width: 100%;
+    background: none;
+    border: 1px solid lightgray;
+    outline: 0;
+    cursor: pointer;
+  }
+  </style>
+  
